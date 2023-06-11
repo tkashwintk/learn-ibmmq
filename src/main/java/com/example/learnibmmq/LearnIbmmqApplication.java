aa16bdb7a24914fc5c52864461e99db6f0beb35b
Ashwin Tk <ashwintkkerala@gmail.com>
2023-06-11 14:18:30 +0200

    IBM MQ demo

---
+++ src/main/java/com/example/learnibmmq/LearnIbmmqApplication.java
@@ -0,0 +1,46 @@
+package com.example.learnibmmq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableJms
+public class LearnIbmmqApplication {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnIbmmqApplication.class, args);
+	}
+
+	@GetMapping("send")
+	String send(){
+		try{
+			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+			return "OK";
+		}catch(JmsException ex){
+			ex.printStackTrace();
+			return "FAIL";
+		}
+	}
+
+	@GetMapping("recv")
+	String recv(){
+		try{
+			return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+		}catch(JmsException ex){
+			ex.printStackTrace();
+			return "FAIL";
+		}
+	}
+
+}
---
+++ src/test/java/com/example/learnibmmq/LearnIbmmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnIbmmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
